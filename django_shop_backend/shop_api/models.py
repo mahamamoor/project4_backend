e500c63228e34fdb8857463768019ac316f480cf
Maha Mamoor <mamoorm@gmail.com>
2022-06-13 14:11:59 -0400

    setup django backend framework

--- django_shop_backend/shop_api/models.py
+++ django_shop_backend/shop_api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Shop(models.Model):
+    title = models.CharField(max_length=32)
+    description = models.CharField(max_length=50)
+    image = models.CharField(max_length=50)
+    price = models.IntegerField()
